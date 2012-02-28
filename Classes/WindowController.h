432a04843fbedf90db5830c1f914d9fdb4ec8d11
Bryan Veloso <bryan@revyver.com>
2012-02-27 21:45:25 -0800

    Creating new methods related to UI construction.

--- Classes/WindowController.h
+++ Classes/WindowController.h
@@ -8,7 +8,7 @@
 
 #import <Foundation/Foundation.h>
 
-@interface WindowController : NSWindowController <NSWindowDelegate>
+@interface WindowController : NSWindowController 
 
 + (WindowController *)windowController;
 
--- Classes/WindowController.m
+++ Classes/WindowController.m
@@ -10,6 +10,11 @@
 
 #import "INAppStoreWindow.h"
 
+@interface WindowController () <NSWindowDelegate>
+- (void)composeInterface;
+- (void)layoutTitleBarSegmentedControls;
+@end
+
 @implementation WindowController
 
 + (WindowController *)windowController
@@ -17,14 +22,30 @@
     return [[WindowController alloc] initWithWindowNibName:@"Window"];
 }
 
-- (void)windowDidLoad
+- (void)awakeFromNib
 {
-    [super windowDidLoad];
-    
+    [super awakeFromNib];
+    [self composeInterface];
+}
+
+- (void)composeInterface
+{
     // Customize INAppStoreWindow.
     INAppStoreWindow *window = (INAppStoreWindow *)[self window];
     window.titleBarHeight = 40.0f;
     window.trafficLightButtonsLeftMargin = 7.0f;
+    
+    [self layoutTitleBarSegmentedControls];
+}
+
+- (void)layoutTitleBarSegmentedControls
+{
+    // - NOOP -
+    // Implements a very crude NSSegmentedControl, used to switch between the 
+    // album view of the track currently opened and the listening statistics 
+    // for that track.
+    INAppStoreWindow *window = (INAppStoreWindow *)[self window];
+    NSView *titleBarView = [window titleBarView];
 }
 
 @end
