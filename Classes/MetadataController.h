9bcdde835cff6fa2d318f56c43b98ce2096bdac0
Bryan Veloso <bryan@revyver.com>
2012-02-29 00:44:36 -0800

    Moving the metadata methods out of AppDelegate.

--- Classes/MetadataController.h
+++ Classes/MetadataController.h
@@ -3,11 +3,15 @@
 //  Perpetual
 //
 //  Created by Bryan Veloso on 2/29/12.
-//  Copyright (c) 2012 Afonso Wilsson. All rights reserved.
+//  Copyright (c) 2012 Revyver, Inc. All rights reserved.
 //
 
 #import <Foundation/Foundation.h>
 
 @interface MetadataController : NSObject
+
++ (MetadataController *)metadataController;
+
+- (void)fetchMetadataForURL:(NSURL *)fileURL;
 
 @end
