6a03a3dece2998d9e4fa522027fee02d2c344f79
Bryan Veloso <bryan@revyver.com>
2012-02-29 00:43:30 -0800

    Adding a MetadataController to handle... metadata.

---
+++ Classes/MetadataController.m
@@ -0,0 +1,13 @@
+//
+//  MetadataController.m
+//  Perpetual
+//
+//  Created by Bryan Veloso on 2/29/12.
+//  Copyright (c) 2012 Afonso Wilsson. All rights reserved.
+//
+
+#import "MetadataController.h"
+
+@implementation MetadataController
+
+@end
