92861b2987ca07cae417f8002b56d5bc6b6e5419
Bryan Veloso <bryan@revyver.com>
2012-02-29 00:28:02 -0800

    OMG, adding a Track model.

---
+++ Classes/Track.h
@@ -0,0 +1,13 @@
+//
+//  Track.h
+//  Perpetual
+//
+//  Created by Bryan Veloso on 2/29/12.
+//  Copyright (c) 2012 Afonso Wilsson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Track : NSObject
+
+@end
---
+++ Classes/Track.m
@@ -0,0 +1,13 @@
+//
+//  Track.m
+//  Perpetual
+//
+//  Created by Bryan Veloso on 2/29/12.
+//  Copyright (c) 2012 Afonso Wilsson. All rights reserved.
+//
+
+#import "Track.h"
+
+@implementation Track
+
+@end
