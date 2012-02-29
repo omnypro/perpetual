0a355556ec0f55c4fae773a761e02bad87bf2557
Bryan Veloso <bryan@revyver.com>
2012-02-28 23:28:09 -0800

    Afonso Wilsson (who the fuck is that?) -> Revyver, Inc.

--- Classes/PlaybackController.m
+++ Classes/PlaybackController.m
@@ -3,7 +3,7 @@
 //  Perpetual
 //
 //  Created by Bryan Veloso on 2/28/12.
-//  Copyright (c) 2012 Afonso Wilsson. All rights reserved.
+//  Copyright (c) 2012 Revyver, Inc. All rights reserved.
 //
 
 #import "PlaybackController.h"
