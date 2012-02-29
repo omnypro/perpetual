72b04fdcc43cf51ed5fc408cd6e1393f041f0410
Bryan Veloso <bryan@revyver.com>
2012-02-29 00:28:59 -0800

    Adding initWithFileURL.

--- Classes/Track.h
+++ Classes/Track.h
@@ -3,11 +3,19 @@
 //  Perpetual
 //
 //  Created by Bryan Veloso on 2/29/12.
-//  Copyright (c) 2012 Afonso Wilsson. All rights reserved.
+//  Copyright (c) 2012 Revyver, Inc. All rights reserved.
 //
 
-#import <Foundation/Foundation.h>
+#import <Cocoa/Cocoa.h>
+#import <QTKit/QTKit.h>
 
 @interface Track : NSObject
+
+@property (retain) QTMovie *asset;
+@property (assign) QTTime duration;
+@property (assign) QTTime startTime;
+@property (assign) QTTime endTime;
+
+- (id)initWithFileURL:(NSURL *)fileURL;
 
 @end
--- Classes/Track.m
+++ Classes/Track.m
@@ -3,11 +3,38 @@
 //  Perpetual
 //
 //  Created by Bryan Veloso on 2/29/12.
-//  Copyright (c) 2012 Afonso Wilsson. All rights reserved.
+//  Copyright (c) 2012 Revyver, Inc. All rights reserved.
 //
 
 #import "Track.h"
 
 @implementation Track
+
+@synthesize asset = _asset;
+@synthesize duration = _duration;
+@synthesize startTime = _startTime;
+@synthesize endTime = _endTime;
+
+- (id)initWithFileURL:(NSURL *)fileURL
+{
+    self = [super init];
+    if (!self) {
+        NSLog(@"Could not initialize track.");
+        return nil;
+    }
+    
+    NSError *err = nil;
+    self.asset = [[QTMovie alloc] initWithURL:fileURL error:&err];
+    if (self.asset == nil) {
+        NSLog(@"%@", err);
+        return nil;
+    }
+    
+    self.duration = [self.asset duration];
+    self.startTime = QTMakeTime(0.0, self.duration.timeScale);
+    self.endTime = self.duration;
+    
+    return self;
+}
 
 @end
