fdda458dc13356f0d9172f7bf26fe395cfcf39c3
Bryan Veloso <bryan@revyver.com>
2012-02-28 23:26:51 -0800

    Adding a new PlaybackController.

---
+++ Classes/PlaybackController.h
@@ -0,0 +1,15 @@
+//
+//  PlaybackController.h
+//  Perpetual
+//
+//  Created by Bryan Veloso on 2/28/12.
+//  Copyright (c) 2012 Afonso Wilsson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface PlaybackController : NSObject
+
++ (PlaybackController *)playbackController;
+
+@end
---
+++ Classes/PlaybackController.m
@@ -0,0 +1,18 @@
+//
+//  PlaybackController.m
+//  Perpetual
+//
+//  Created by Bryan Veloso on 2/28/12.
+//  Copyright (c) 2012 Afonso Wilsson. All rights reserved.
+//
+
+#import "PlaybackController.h"
+
+@implementation PlaybackController
+
++ (PlaybackController *)playbackController
+{
+    return [[PlaybackController alloc] init];
+}
+
+@end
