5c4de2ee45eb3a114b9177dff6bb38cba8a84b36
Bryan Veloso <bryan@revyver.com>
2012-03-03 18:12:08 -0800

    Adding a NSString category for converting an NSTimeInterval to minutes and seconds.

---
+++ Categories/NSString+TimeConversion.h
@@ -0,0 +1,13 @@
+//
+//  NSString+TimeConversion.h
+//  Perpetual
+//
+//  Created by Bryan Veloso on 3/3/12.
+//  Copyright (c) 2012 Afonso Wilsson. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface NSString (TimeConversion)
+
+@end
---
+++ Categories/NSString+TimeConversion.m
@@ -0,0 +1,13 @@
+//
+//  NSString+TimeConversion.m
+//  Perpetual
+//
+//  Created by Bryan Veloso on 3/3/12.
+//  Copyright (c) 2012 Afonso Wilsson. All rights reserved.
+//
+
+#import "NSString+TimeConversion.h"
+
+@implementation NSString (TimeConversion)
+
+@end
