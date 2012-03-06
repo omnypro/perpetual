c57ac80d7765af225a0db6a261a6a5193b126c6d
Bryan Veloso <bryan@revyver.com>
2012-03-05 23:45:33 -0800

    Adding a TooltipWindow skeleton.

---
+++ Classes/Views/TooltipWindow.h
@@ -0,0 +1,20 @@
+//
+//  TooltipWindow.h
+//  Perpetual
+//
+//  Created by Bryan Veloso on 3/5/12.
+//  Copyright (c) 2012 Revyver, Inc. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface TooltipWindow : NSWindow
+
+@property (nonatomic, strong) NSTextField *time;
+
+- (void)show;
+- (void)hide;
+- (void)updatePosition:(float)y;
+- (void)setString:(NSString *)stringValue;
+
+@end
---
+++ Classes/Views/TooltipWindow.m
@@ -0,0 +1,40 @@
+//
+//  TooltipWindow.m
+//  Perpetual
+//
+//  Created by Bryan Veloso on 3/5/12.
+//  Copyright (c) 2012 Revyver, Inc. All rights reserved.
+//
+
+#import "TooltipWindow.h"
+
+@implementation TooltipWindow
+
+@synthesize time = _time;
+
+- (id)initWithContentRect:(NSRect)contentRect styleMask:(NSUInteger)aStyle backing:(NSBackingStoreType)bufferingType defer:(BOOL)flag
+{
+    
+}
+
+- (void)show
+{
+    
+}
+
+- (void)hide
+{
+    
+}
+
+- (void)updatePosition:(float)y
+{
+    
+}
+
+- (void)setString:(NSString *)stringValue
+{
+    
+}
+
+@end
