7c2cdfbb8d6834f706c0ac70846a4ee4ef41a676
Bryan Veloso <bryan@revyver.com>
2012-03-14 01:19:01 -0700

    Adding the new window controller.

---
+++ Classes/controllers/AboutWindowController.h
@@ -0,0 +1,13 @@
+//
+//  AboutWindowController.h
+//  Perpetual
+//
+//  Created by Bryan Veloso on 3/14/12.
+//  Copyright (c) 2012 Revyver, Inc. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+
+@interface AboutWindowController : NSWindowController
+
+@end
---
+++ Classes/controllers/AboutWindowController.m
@@ -0,0 +1,34 @@
+//
+//  AboutWindowController.m
+//  Perpetual
+//
+//  Created by Bryan Veloso on 3/14/12.
+//  Copyright (c) 2012 Revyver, Inc. All rights reserved.
+//
+
+#import "AboutWindowController.h"
+
+@interface AboutWindowController ()
+
+@end
+
+@implementation AboutWindowController
+
+- (id)initWithWindow:(NSWindow *)window
+{
+    self = [super initWithWindow:window];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+- (void)windowDidLoad
+{
+    [super windowDidLoad];
+    
+    // Implement this method to handle any initialization after your window controller's window has been loaded from its nib file.
+}
+
+@end
