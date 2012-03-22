71990243e2d6fdc9840b9da65ed317e50b171b87
Bryan Veloso <bryan@revyver.com>
2012-03-21 23:57:08 -0700

    Adding a DropZoneViewController.

---
+++ Classes/controllers/DropZoneViewController.m
@@ -0,0 +1,27 @@
+//
+//  DropZoneViewController.m
+//  Perpetual
+//
+//  Created by Bryan Veloso on 3/21/12.
+//  Copyright (c) 2012 Revyver, Inc. All rights reserved.
+//
+
+#import "DropZoneViewController.h"
+
+@interface DropZoneViewController ()
+
+@end
+
+@implementation DropZoneViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
