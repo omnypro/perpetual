e2ce693bdf8216b84201bf807a35a0dfb006772d
Bryan Veloso <bryan@revyver.com>
2012-03-23 00:11:55 -0700

    Hooking up the outlets.

--- Classes/controllers/PlayerViewController.h
+++ Classes/controllers/PlayerViewController.h
@@ -8,6 +8,22 @@
 
 #import <Cocoa/Cocoa.h>
 
+@class SMDoubleSlider;
+@class WebView;
+
 @interface PlayerViewController : NSViewController
+
+// Cover and Statistics Display
+@property (weak) IBOutlet WebView *webView;
+
+// Track Metadata Displays
+@property (weak) IBOutlet NSTextField *trackTitle;
+@property (weak) IBOutlet NSTextField *trackSubtitle;
+@property (weak) IBOutlet NSTextField *currentTime;
+@property (weak) IBOutlet NSTextField *rangeTime;
+
+// Sliders and Progress Bar
+@property (weak) IBOutlet NSLevelIndicator *progressBar;
+@property (weak) IBOutlet SMDoubleSlider *rangeSlider;
 
 @end
--- Classes/controllers/PlayerViewController.m
+++ Classes/controllers/PlayerViewController.m
@@ -14,6 +14,19 @@
 
 @implementation PlayerViewController
 
+// Cover and Statistics Display
+@synthesize webView = _webView;
+
+// Track Metadata Displays
+@synthesize trackTitle = _trackTitle;
+@synthesize trackSubtitle = _trackSubtitle;
+@synthesize currentTime = _currentTime;
+@synthesize rangeTime = _rangeTime;
+
+// Sliders and Progress Bar
+@synthesize progressBar = _progressBar;
+@synthesize rangeSlider = _rangeSlider;
+
 - (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
 {
     self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
