e9e533e69a47e30831eca9522317282d220981a9
Bryan Veloso <bryan@revyver.com>
2012-03-22 23:48:22 -0700

    Adding a PlayerViewController to hold all of our player controls.

---
+++ Classes/controllers/PlayerViewController.m
@@ -0,0 +1,27 @@
+//
+//  PlayerViewController.m
+//  Perpetual
+//
+//  Created by Bryan Veloso on 3/22/12.
+//  Copyright (c) 2012 Revyver, Inc. All rights reserved.
+//
+
+#import "PlayerViewController.h"
+
+@interface PlayerViewController ()
+
+@end
+
+@implementation PlayerViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Initialization code here.
+    }
+    
+    return self;
+}
+
+@end
