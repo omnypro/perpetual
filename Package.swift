763c68986320b01923c1ba725c7c0047f5a8c111
Bryan Veloso <bryan@velo.so>
2025-05-18 22:20:17 -0700

    Add AudioKit to Package.swift.

--- Package.swift
+++ Package.swift
@@ -2,25 +2,32 @@
 import PackageDescription
 
 let package = Package(
-  name: "Perpetual",
-  platforms: [
-    .macOS(.v14)
-  ],
-  products: [
-    .executable(name: "Perpetual", targets: ["Perpetual"])
-  ],
-  dependencies: [],
-  targets: [
-    .executableTarget(
-      name: "Perpetual",
-      dependencies: [],
-      path: "Sources/Perpetual",
-      resources: [.process("Resources")]
-    ),
-    .testTarget(
-      name: "PerpetualTests",
-      dependencies: ["Perpetual"],
-      path: "Tests"
-    )
-  ]
+    name: "Perpetual",
+    platforms: [
+        .macOS(.v14)
+    ],
+    products: [
+        .executable(name: "Perpetual", targets: ["Perpetual"])
+    ],
+    dependencies: [
+        .package(url: "https://github.com/AudioKit/AudioKit.git", .upToNextMajor(from: "5.6.5")),
+    ],
+    targets: [
+        .executableTarget(
+            name: "Perpetual",
+            dependencies: [
+                .product(name: "AudioKit", package: "AudioKit"),
+            ],
+            path: "Sources/Perpetual",
+            resources: [
+                .process("Resources"),
+                .process("Info.plist")
+            ]
+        ),
+        .testTarget(
+            name: "PerpetualTests",
+            dependencies: ["Perpetual"],
+            path: "Tests"
+        ),
+    ]
 )
